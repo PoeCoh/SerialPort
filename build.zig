6c0fee32a04bfa866bbd55d584626fe531a62da5
Ben Jordan <ben@poecoh.com>
2024-07-29 15:11:27 -0400

    Fixed lib path

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .name = "SerialPort",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/SerialPort.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -30,7 +30,7 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
-        .name = "SerialPort",
+        .name = "SerialPortExe",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
